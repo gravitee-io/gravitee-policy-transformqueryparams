7773f767536e6075cf0f2fe3c44f825e2e153674
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2023-11-20 22:58:49 +0100

    refactor: use lombok and refactor tests

--- src/test/java/io/gravitee/policy/transformqueryparams/TransformQueryParametersPolicyTest.java
+++ src/test/java/io/gravitee/policy/transformqueryparams/TransformQueryParametersPolicyTest.java
@@ -76,221 +76,239 @@ public class TransformQueryParametersPolicyTest {
 
     @Test
     public void shouldAddSimpleParam() {
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param = new HttpQueryParameter();
-        param.setName("foo");
-        param.setValue("bar");
-        parameters.add(param);
-        when(configuration.isClearAll()).thenReturn(false);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo").value("bar").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo", "bar");
+            }
+        };
 
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("foo"));
-        assertEquals(1, request.parameters().get("foo").size());
-        assertEquals("bar", request.parameters().get("foo").get(0));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
     }
 
     @Test
     public void shouldAddSimpleParamAndKeepExistingParams() {
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param = new HttpQueryParameter();
-        param.setName("foo");
-        param.setValue("bar");
-        parameters.add(param);
-        when(configuration.isClearAll()).thenReturn(false);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        requestParams.add("existing", "value");
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo").value("bar").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("existing", "value");
+            }
+        };
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("existing", "value");
+                add("foo", "bar");
+            }
+        };
 
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("foo"));
-        assertEquals(1, request.parameters().get("foo").size());
-        assertEquals("bar", request.parameters().get("foo").get(0));
-        assertNotNull(request.parameters().get("existing"));
-        assertEquals(1, request.parameters().get("existing").size());
-        assertEquals("value", request.parameters().get("existing").get(0));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
     }
 
     @Test
     public void shouldAddEncodedParam() {
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param = new HttpQueryParameter();
-        param.setName("foo%20name");
-        param.setValue("bar%20name");
-        parameters.add(param);
-        when(configuration.isClearAll()).thenReturn(false);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo%20name").value("bar%20name").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo%20name", "bar%20name");
+            }
+        };
 
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("foo%20name"));
-        assertEquals(1, request.parameters().get("foo%20name").size());
-        assertEquals("bar%20name", request.parameters().get("foo%20name").get(0));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
     }
 
     @Test
     public void shouldAddUnencodedParam() {
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param = new HttpQueryParameter();
-        param.setName("foo&name");
-        param.setValue("bar'name&=3");
-        parameters.add(param);
-        when(configuration.isClearAll()).thenReturn(false);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo&name").value("bar'name&=3").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo&name", "bar'name&=3");
+            }
+        };
 
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("foo&name"));
-        assertEquals(1, request.parameters().get("foo&name").size());
-        assertEquals("bar'name&=3", request.parameters().get("foo&name").get(0));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
     }
 
     @Test
     public void shouldEncodeWhitespaceParam() {
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param = new HttpQueryParameter();
-        param.setName("foo name");
-        param.setValue("bar name");
-        parameters.add(param);
-        when(configuration.isClearAll()).thenReturn(false);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo name").value("bar name").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo%20name", "bar%20name");
+            }
+        };
 
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("foo%20name"));
-        assertEquals(1, request.parameters().get("foo%20name").size());
-        assertEquals("bar%20name", request.parameters().get("foo%20name").get(0));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
     }
 
     @Test
     public void shouldClearAll() {
-        when(configuration.isClearAll()).thenReturn(true);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        requestParams.add("foo", "bar");
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>();
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo% 20name", "bar%20name");
+            }
+        };
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>();
 
-        assertFalse(request.parameters().isEmpty());
-
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertTrue(request.parameters().isEmpty());
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, true);
     }
 
     @Test
     public void shouldOverride() {
-        when(configuration.isClearAll()).thenReturn(false);
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param = new HttpQueryParameter();
-        param.setName("foo");
-        param.setValue("newbar");
-        parameters.add(param);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
-        when(configuration.isClearAll()).thenReturn(false);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        requestParams.add("foo", "bar");
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo").value("newbar").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo", "bar");
+            }
+        };
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo", "newbar");
+            }
+        };
 
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("foo"));
-        assertEquals(1, request.parameters().get("foo").size());
-        assertEquals("newbar", request.parameters().get("foo").get(0));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
     }
 
     @Test
     public void shouldRemoveAllAndAdd() {
-        when(configuration.isClearAll()).thenReturn(true);
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param = new HttpQueryParameter();
-        param.setName("foo");
-        param.setValue("bar");
-        parameters.add(param);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        requestParams.add("old", "value");
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo").value("bar").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("old", "value");
+            }
+        };
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo", "bar");
+            }
+        };
 
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("foo"));
-        assertEquals(1, request.parameters().get("foo").size());
-        assertEquals("bar", request.parameters().get("foo").get(0));
-        assertNull(request.parameters().get("old"));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, true);
     }
 
     @Test
     public void shouldRemoveParam() {
-        when(configuration.isClearAll()).thenReturn(false);
-        List<String> parameters = new ArrayList<>();
-        String param = "foo";
-        parameters.add(param);
-        when(configuration.getRemoveQueryParameters()).thenReturn(parameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        requestParams.add("existing", "value");
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>();
+        List<String> removeQueryParameters = new ArrayList<>() {
+            {
+                add("foo");
+            }
+        };
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo", "bar");
+                add("old", "value");
+            }
+        };
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("old", "value");
+            }
+        };
 
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("existing"));
-        assertNull(request.parameters().get("foo"));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
     }
 
     @Test
-    public void shouldVerifyOrderAddAndRemoveParam() {
-        when(configuration.isClearAll()).thenReturn(false);
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param = new HttpQueryParameter();
-        param.setName("foo");
-        param.setValue("bar");
-        parameters.add(param);
-        when(configuration.isClearAll()).thenReturn(false);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
+    public void shouldVerifyOrderAddThenRemoveParam() {
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo").value("bar").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>() {
+            {
+                add("foo");
+            }
+        };
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("existing", "value");
+            }
+        };
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("existing", "value");
+            }
+        };
 
-        List<String> removeparameters = new ArrayList<>();
-        String removeparam = "foo";
-        removeparameters.add(removeparam);
-        when(configuration.getRemoveQueryParameters()).thenReturn(removeparameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        requestParams.add("existing", "value");
-        when(request.parameters()).thenReturn(requestParams);
-
-        policy.onRequest(request, response, executionContext, policyChain);
-
-        assertNotNull(request.parameters().get("existing"));
-        assertNull(request.parameters().get("foo"));
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
     }
 
     @Test
     public void shouldAddParamDoubleTime() {
-        List<HttpQueryParameter> parameters = new ArrayList<>();
-        HttpQueryParameter param1 = new HttpQueryParameter();
-        param1.setName("foo");
-        param1.setValue("bar1");
-        parameters.add(param1);
-        HttpQueryParameter param2 = new HttpQueryParameter();
-        param2.setName("foo");
-        param2.setValue("bar2");
-        parameters.add(param2);
-        when(configuration.isClearAll()).thenReturn(false);
-        when(configuration.getAddQueryParameters()).thenReturn(parameters);
-        MultiValueMap requestParams = new LinkedMultiValueMap();
-        when(request.parameters()).thenReturn(requestParams);
+        List<HttpQueryParameter> addQueryParameters = new ArrayList<>() {
+            {
+                add(HttpQueryParameter.builder().name("foo").value("bar").build());
+                add(HttpQueryParameter.builder().name("foo").value("bar2").build());
+            }
+        };
+        List<String> removeQueryParameters = new ArrayList<>();
+        MultiValueMap<String, String> requestQueryParams = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> expectedQueryParams = new LinkedMultiValueMap<>() {
+            {
+                add("foo", "bar2");
+            }
+        };
+
+        shouldTest(requestQueryParams, addQueryParameters, removeQueryParameters, expectedQueryParams, false);
+    }
+
+    private void shouldTest(
+        MultiValueMap<String, String> requestQueryParams,
+        List<HttpQueryParameter> addQueryParameters,
+        List<String> removeQueryParameters,
+        MultiValueMap<String, String> expectedQueryParams,
+        boolean clearAll
+    ) {
+        when(configuration.isClearAll()).thenReturn(clearAll);
+        when(configuration.getAddQueryParameters()).thenReturn(addQueryParameters);
+        when(configuration.getRemoveQueryParameters()).thenReturn(removeQueryParameters);
+        when(request.parameters()).thenReturn(requestQueryParams);
 
         policy.onRequest(request, response, executionContext, policyChain);
 
-        assertNotNull(request.parameters().get("foo"));
-        assertEquals(1, request.parameters().get("foo").size());
-        assertEquals("bar2", request.parameters().get("foo").get(0));
+        assertEquals(expectedQueryParams, request.parameters());
     }
 }
